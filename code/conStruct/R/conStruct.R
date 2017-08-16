34eaff782629dd0e34071c40a7d8b8f168c151f9
Gideon Bradburd <bradburd@user-25c879.user.msu.edu>
2017-08-16 11:09:09 -0400

    no unfolding freqs

--- code/conStruct/R/conStruct.R
+++ code/conStruct/R/conStruct.R
@@ -1,4 +1,4 @@
-#'@export
+
 validate.data.list <- function(data.block){
 	if(!"spatial" %in% names(data.block)){
 		stop("\nUser must specify a \"spatial\" option\n\n")
@@ -18,7 +18,7 @@ validate.data.list <- function(data.block){
 	return(invisible("list elements validated"))	
 }
 
-#'@export
+
 validate.n.samples <- function(data.block){
 	n.samples <- data.block$N
 	n.samples <- c(data.block$N,nrow(data.block$obsCov))
@@ -32,7 +32,7 @@ validate.n.samples <- function(data.block){
 	return(invisible("n.samples validated"))
 }
 
-#'@export
+
 validate.model <- function(data.block){
 	if(data.block$spatial){
 		if(is.null(data.block$geoDist)){
@@ -54,19 +54,19 @@ validate.model <- function(data.block){
 	return(invisible("model validated"))
 }
 
-#'@export
+
 make.data.block.S3 <- function(data.block){
 	data.block <- data.block
 	class(data.block) <- "data.block"
 	return(data.block)
 }
 
-#'@export
+
 print.data.block <- function(data.block){
 	print(str(data.block,max.level=1))
 }
 
-#'@export
+
 validate.data.block <- function(data.block){
 	message("\nchecking data.block\n")
 		validate.data.list(data.block)
@@ -85,7 +85,7 @@ validate.data.block <- function(data.block){
 	return(data.block)
 }
 
-#'@export
+
 make.stan.code.block <- function(spatial,n.clusters){
 	stan.code.block.name <- "stan.block"
 	if(n.clusters == 1){
@@ -100,41 +100,31 @@ make.stan.code.block <- function(spatial,n.clusters){
 	return(get(stan.code.block.name))
 }
 
-#'@export
+
 make.freq.data.list.S3 <- function(freq.data){
 	freq.data <- freq.data
 	class(freq.data) <- "freq.data"
 	return(freq.data)
 }
 
-#'@export
+
 print.freq.data <- function(freq.data){
 	print(str(freq.data,max.level=1))
 }
 
-#'@export
-unfold.freqs <- function(freqs){
-	N <- nrow(freqs)
-	L <- ncol(freqs)
-	unfold <- runif(L,0,1) > 0.5
-	freqs[,unfold] <- 1 - freqs[,unfold]
-	return(freqs)
-}
-
-#'@export
 identify.invar.sites <- function(freqs){
 	invar <- length(unique(freqs[which(!is.na(freqs))])) == 1
 	return(invar)
 }
 
-#'@export
+
 drop.invars <- function(freqs){
 	invars <- apply(freqs,2,identify.invar.sites)
 	freqs <- freqs[,!invars]
 	return(freqs)
 }
 
-#'@export
+
 identify.missing.sites <- function(freqs){
 	n.samples <- length(freqs)
 	missing <- FALSE
@@ -144,22 +134,28 @@ identify.missing.sites <- function(freqs){
 	return(missing)
 }
 
-#'@export
+
 drop.missing <- function(freqs){
 	missing <- apply(freqs,2,identify.missing.sites)
 	freqs <- freqs[,!missing]
 	return(freqs)
 }
 
-#'@export
+calc.covariance <- function(freqs){
+	x <- t(freqs)
+	allelic.covariance <- cov(x,use="pairwise.complete.obs") - 
+							(1/2) * outer( colMeans(x,na.rm=TRUE), 1-colMeans(x,na.rm=TRUE), "*" ) -
+							(1/2) * outer(1-colMeans(x,na.rm=TRUE), colMeans(x,na.rm=TRUE), "*") + 1/4
+	diag(allelic.covariance) <- 1/4
+	return(allelic.covariance)
+}
+
+
 process.freq.data <- function(freqs){
-	freqs <- unfold.freqs(freqs)
 	freqs <- drop.invars(freqs)
 	freqs <- drop.missing(freqs)
 	n.loci <- ncol(freqs)
-	obsCov <- cov(t(freqs),use="pairwise.complete.obs")
-	mean.freqs <- rowMeans(freqs,na.rm=TRUE)
-	diag(obsCov) <- mean.freqs * (1 - mean.freqs)
+	obsCov <- calc.covariance(freqs)
 	freq.data <- list("freqs" = freqs,
 					  "obsCov" = obsCov,
 					  "n.loci" = n.loci)
@@ -167,8 +163,6 @@ process.freq.data <- function(freqs){
 	return(freq.data)
 }
 
-
-#'@export
 standardize.distances <- function(D){
 	if(!is.null(D)){
 		stdev.D <- sd(D[upper.tri(D)])
@@ -179,7 +173,7 @@ standardize.distances <- function(D){
 	return(std.D)
 }
 
-#'@export
+
 set.temp <- function(temp=NULL){
 	if(is.null(temp)){
 		temp <- 1
@@ -189,7 +183,7 @@ set.temp <- function(temp=NULL){
 	return(temp)
 }
 
-#'@export
+
 make.data.block <- function(K,freq.data,coords,spatial,geoDist=NULL,temp=NULL){
 	data.block <- list("N" = nrow(coords),
 					   "K" = K,
@@ -198,13 +192,13 @@ make.data.block <- function(K,freq.data,coords,spatial,geoDist=NULL,temp=NULL){
 					   "coords" = coords,
 					   "obsCov" = freq.data$obsCov,
 					   "geoDist" = standardize.distances(geoDist),
-					   "varMeanFreqs" = var(apply(freq.data$freqs,2,function(x){mean(x,na.rm=TRUE)})),
+					   "varMeanFreqs" = mean(0.5*colMeans(freqs-0.5,na.rm=TRUE)^2 + 0.5*colMeans(1-freqs-0.5,na.rm=TRUE)^2),
 					   "temp" = set.temp(temp))
 	data.block <- validate.data.block(data.block)
 	return(data.block)
 }
 
-#'@export
+
 check.call <- function(args){
 	if(args[["spatial"]] != TRUE & args[["spatial"]] != FALSE){
 		stop("\nyou have specified an invalid value for the \"spatial\" argument \n")
@@ -224,12 +218,11 @@ check.call <- function(args){
 	return(invisible("args checked"))		
 }
 
-#'@export
+
 conStruct <- function(spatial=TRUE,K,freqs,geoDist=NULL,temp=NULL,coords,prefix="",n.chains=1,n.iter=1e3,make.figs=TRUE,save.files=TRUE){
 	call.check <- check.call(args <- as.list(environment()))
 	#validate data block
 	freq.data <- process.freq.data(freqs)
-		save(freq.data,file=paste0(prefix,"_freq.data.Robj"))
 	data.block <- make.data.block(K,freq.data,coords,spatial,geoDist,temp)
 		if(save.files){
 			save(data.block,file=paste0(prefix,"_data.block.Robj"))
@@ -252,14 +245,16 @@ conStruct <- function(spatial=TRUE,K,freqs,geoDist=NULL,temp=NULL,coords,prefix=
 			save(model.fit,file=paste(prefix,"model.fit.Robj",sep="_"))
 		}
 	conStruct.results <- get.conStruct.results(data.block,model.fit,n.chains)
-		save(conStruct.results,file=paste(prefix,"conStruct.results.Robj",sep="_"))
+		if(save.files){
+			save(conStruct.results,file=paste(prefix,"conStruct.results.Robj",sep="_"))
+		}
 	if(make.figs){
 		make.all.the.plots(conStruct.results,n.chains,data.block,prefix,cluster.colors=NULL)
 	}
 	return(conStruct.results)
 }
 
-#'@export
+
 get.conStruct.results <- function(data.block,model.fit,n.chains){
 	conStruct.results <- setNames(
 						lapply(1:n.chains,
@@ -270,14 +265,14 @@ get.conStruct.results <- function(data.block,model.fit,n.chains){
 	return(conStruct.results)
 }
 
-#'@export
+
 get.MAP.iter <- function(model.fit,chain.no){
 	lpd <- get_logposterior(model.fit)
 	MAP.iter <- lapply(lpd,which.max)[[chain.no]]
 	return(MAP.iter)
 }
 
-#'@export
+
 get.admix.props <- function(model.fit,chain.no,N,n.clusters){
 	# recover()
 	admix.props <- array(1,dim=c(model.fit@sim$n_save[chain.no],N,n.clusters))
@@ -291,7 +286,7 @@ get.admix.props <- function(model.fit,chain.no,N,n.clusters){
 	return(admix.props)
 }
 
-#'@export
+
 get.par.cov <- function(model.fit,chain.no,N){
 	par.cov <- array(NA,dim=c(model.fit@sim$n_save[chain.no],N,N))
 	for(i in 1:N){
@@ -303,13 +298,13 @@ get.par.cov <- function(model.fit,chain.no,N){
 	return(par.cov)
 }
 
-#'@export
+
 get.nuggets <- function(model.fit,chain.no,N){
 	nuggets <- extract(model.fit,pars="nugget",inc_warmup=TRUE,permuted=FALSE)[,chain.no,]
 	return(nuggets)
 }
 
-#'@export
+
 get.gamma <- function(model.fit,chain.no){
 	gamma <- extract(model.fit,pars="gamma",inc_warmup=TRUE,permuted=FALSE)[,chain.no,]
 	return(gamma)
@@ -322,7 +317,7 @@ get.null.alpha.params <- function(n.iter){
 	return(alpha.params)	
 }
 
-#'@export
+
 get.alpha.params <- function(model.fit,chain.no,cluster,n.clusters){
 	alpha.pars <- model.fit@model_pars[grepl("alpha",model.fit@model_pars)]
 	if(length(alpha.pars) !=0 ){
@@ -354,7 +349,7 @@ get.null.mu <- function(n.iter){
 	return(mu)
 }
 
-#'@export
+
 get.cluster.mu <- function(model.fit,chain.no,cluster){
 	has.mu <- any(grepl("mu",model.fit@model_pars))
 	if(has.mu){
@@ -367,7 +362,7 @@ get.cluster.mu <- function(model.fit,chain.no,cluster){
 	return(mu)
 }
 
-#'@export
+
 get.cov.function <- function(data.block){
 	if(data.block$K == 1){
 		if(data.block$spatial){
@@ -398,7 +393,7 @@ get.cov.function <- function(data.block){
 	return(cov.func)
 }
 
-#'@export
+
 get.cluster.cov <- function(cluster.params,data.block,n.iter){
 	cov.function <- get.cov.function(data.block)
 	cluster.cov <- lapply(1:n.iter,
@@ -410,7 +405,7 @@ get.cluster.cov <- function(cluster.params,data.block,n.iter){
 	return(cluster.cov)
 }
 
-#'@export
+
 get.cluster.params <- function(model.fit,data.block,chain.no,cluster,n.clusters,n.iter){
 	cluster.params <- list()
 	cluster.params <- get.alpha.params(model.fit,chain.no,cluster,n.clusters)
@@ -420,7 +415,7 @@ get.cluster.params <- function(model.fit,data.block,chain.no,cluster,n.clusters,
 	return(cluster.params)
 }
 
-#'@export
+
 get.cluster.params.list <- function(model.fit,data.block,chain.no,n.iter){
 	cluster.params <- setNames(
 						lapply(1:data.block$K,
@@ -432,19 +427,19 @@ get.cluster.params.list <- function(model.fit,data.block,chain.no,n.iter){
 	return(cluster.params)
 }
 
-#'@export
+
 make.cluster.params.S3 <- function(cluster.params){
 	cluster.params <- cluster.params
 	class(cluster.params) <- "cluster.params"
 	return(cluster.params)
 }
 
-#'@export
+
 print.cluster.params <- function(cluster.params){
 	print(str(cluster.params,max.level=1))
 }
 
-#'@export
+
 index.MAP <- function(param,MAP.iter){
 	if(class(param) == "numeric"){
 		MAP.param <- param[MAP.iter]
@@ -467,37 +462,37 @@ index.MAP <- function(param,MAP.iter){
 	return(MAP.param)
 }
 
-#'@export
+
 index.MAP.cluster.params <- function(cluster.params,MAP.iter){
 	MAP.cluster.params <- lapply(cluster.params,index.MAP,MAP.iter)
 	return(MAP.cluster.params)
 }
 
-#'@export
+
 index.MAP.cluster.params.list <- function(cluster.params.list,MAP.iter){
 	MAP.cluster.params.list <- lapply(cluster.params.list,index.MAP.cluster.params,MAP.iter)
 	return(MAP.cluster.params.list)
 }
 
-#'@export
+
 get.n.iter <- function(model.fit,chain.no){
 	n.iter <- model.fit@sim$n_save[chain.no]
 	return(n.iter)
 }
 
-#'@export
+
 make.conStruct.results.S3 <- function(conStruct.results){
 	conStruct.results <- conStruct.results
 	class(conStruct.results) <- "conStruct.results"
 	return(conStruct.results)
 }
 
-#'@export
+
 print.conStruct.results <- function(conStruct.results){
 	print(str(conStruct.results,max.level=1))
 }
 
-#'@export
+
 get.conStruct.chain.results <- function(data.block,model.fit,chain.no){
 	n.iter <- get.n.iter(model.fit,chain.no)
 	posterior <- list("n.iter" = model.fit@sim$n_save[chain.no],
@@ -514,7 +509,7 @@ get.conStruct.chain.results <- function(data.block,model.fit,chain.no){
 	return(conStruct.results)
 }
 
-#'@export
+
 plot.lpd <- function(conStruct.results){
 	plot(conStruct.results$posterior$lpd,
 			ylab="posterior probability",
@@ -523,7 +518,7 @@ plot.lpd <- function(conStruct.results){
 	return(invisible(0))
 }
 
-#'@export
+
 plot.nuggets <- function(conStruct.results){
 	matplot(conStruct.results$post$nuggets,type='l',
 				main="sample nuggets",
@@ -540,7 +535,7 @@ plot.gamma <- function(conStruct.results){
 	return(invisible(0))
 }
 
-#'@export
+
 get.ylim <- function(cluster.params,n.clusters,param){
 	y.lim <- range(unlist(
 				lapply(
@@ -555,13 +550,13 @@ get.ylim <- function(cluster.params,n.clusters,param){
 	return(y.lim)
 }
 
-#'@export
+
 plot.cluster.param <- function(cluster.param,clst.col){
 	points(cluster.param,type='l',col=clst.col)
 	return(invisible(0))
 }
 
-#'@export
+
 plot.cluster.cov.params <- function(data.block,conStruct.results,cluster.colors){
 	n.clusters <- data.block$K
 	params <- names(conStruct.results$posterior$cluster.params$Cluster_1)[!names(conStruct.results$posterior$cluster.params$Cluster_1)=="cluster.cov"]
@@ -578,7 +573,7 @@ plot.cluster.cov.params <- function(data.block,conStruct.results,cluster.colors)
 	return(invisible(0))
 }
 
-#'@export
+
 plot.admix.props <- function(data.block,conStruct.results,cluster.colors){
 	n.clusters <- data.block$K
 	par(mfrow=c(n.clusters,1),mar=c(3,3,2,2))
@@ -589,7 +584,7 @@ plot.admix.props <- function(data.block,conStruct.results,cluster.colors){
 	return(invisible(0))
 }
 
-#'@export
+
 get.par.cov.CI <- function(data.block,conStruct.results){
 	combns <- gtools::combinations(n=data.block$N,r=2,v=1:data.block$N,repeats.allowed=TRUE)
 	CIs <- lapply(1:nrow(combns),
@@ -598,7 +593,7 @@ get.par.cov.CI <- function(data.block,conStruct.results){
 				})
 	return(CIs)
 }
-#'@export
+
 plot.model.fit.CIs <- function(data.block,conStruct.results){
 	cov.range <- range(c(data.block$obsCov,
 						conStruct.results$posterior$par.cov))
@@ -623,7 +618,7 @@ plot.model.fit.CIs <- function(data.block,conStruct.results){
 	return(invisible("plotted"))
 }
 
-#'@export
+
 plot.model.fit <- function(data.block,conStruct.results){
 	index.mat <- upper.tri(data.block$geoDist, diag = TRUE)
 	cov.range <- range(c(data.block$obsCov,conStruct.results$posterior$par.cov))
@@ -643,7 +638,7 @@ plot.model.fit <- function(data.block,conStruct.results){
 	return(invisible("plotted"))
 }
 
-#'@export
+
 plot.cluster.covariances <- function(data.block,conStruct.results,cluster.colors){
 	ind.mat <- upper.tri(data.block$geoDist,diag=TRUE)
 	order.mat <- order(data.block$geoDist)
@@ -674,7 +669,7 @@ plot.cluster.covariances <- function(data.block,conStruct.results,cluster.colors
 	return(invisible("cluster covs"))	
 }
 
-#'@export
+
 structure.polygon <- function(plotting.admix.props,i,j,use.colors){
 	polygon(x = c(j-1,j,j,j-1),
 			y = c(plotting.admix.props[i,j],
@@ -685,7 +680,7 @@ structure.polygon <- function(plotting.admix.props,i,j,use.colors){
 	return(invisible(j))
 }
 
-#'@export
+
 make.structure.polygon.layer <- function(plotting.admix.props,i,use.colors,sample.order){
 		lapply(1:ncol(plotting.admix.props),function(j){
 			structure.polygon(plotting.admix.props[,sample.order],i,j,use.colors)
@@ -693,7 +688,7 @@ make.structure.polygon.layer <- function(plotting.admix.props,i,use.colors,sampl
 	return(invisible(i))
 }
 
-#'@export
+
 make.structure.plot <- function(data.block,conStruct.results,mar=c(2,4,2,2),sample.order=NULL,cluster.order=NULL,sample.names=NULL,sort.by=NULL,cluster.colors=NULL){
 	par(mar=mar)
 	if(is.null(cluster.order)){
@@ -723,7 +718,7 @@ make.structure.plot <- function(data.block,conStruct.results,mar=c(2,4,2,2),samp
 	return(invisible("plotted"))
 }
 
-#'@export
+
 make.admix.pie.plot <- function(data.block,conStruct.results,cluster.colors,stat,radii=2.7,add=FALSE,title=NULL,x.lim=NULL,y.lim=NULL){
 	if(is.null(data.block$coords)){
 		message("\nuser has not specified sampling coordinates in the data block\n")
@@ -763,7 +758,7 @@ make.admix.pie.plot <- function(data.block,conStruct.results,cluster.colors,stat
 	return(invisible(0))
 }
 
-#'@export
+
 make.all.chain.plots <- function(conStruct.results,chain.no,data.block,prefix,cluster.colors){
 	pdf(file=paste0(prefix,"_trace.plots.chain_",chain.no,".pdf"))
 		plot.lpd(conStruct.results)
@@ -796,7 +791,7 @@ make.all.chain.plots <- function(conStruct.results,chain.no,data.block,prefix,cl
 	return(invisible("made chain plots!"))
 }
 
-#'@export
+
 make.all.the.plots <- function(conStruct.results,n.chains,data.block,prefix,cluster.colors=NULL){
 	if(is.null(cluster.colors)){
 		cluster.colors <- c("blue","red","green","yellow","purple","orange","lightblue","darkgreen","lightblue","gray")
@@ -807,7 +802,7 @@ make.all.the.plots <- function(conStruct.results,n.chains,data.block,prefix,clus
 	return(invisible("made chain plots!"))
 }
 
-#'@export
+
 post.process.par.cov <- function(conStruct.results,samples){
 	pp.cov.list <- lapply(samples,
 							function(i){
@@ -817,13 +812,13 @@ post.process.par.cov <- function(conStruct.results,samples){
 	return(pp.cov.list)
 }
 
-#'@export
+
 log.likelihood <- function(obsCov,inv.par.cov,log.det,n.loci){
 	lnL <- -0.5 * (sum( inv.par.cov * obsCov) + n.loci * log.det)
 	return(lnL)
 }
 
-#'@export
+
 determine.log.shift <- function(chunk.lnls){
 	if(diff(range(unlist(chunk.lnls))) > 700){
 		message("the difference between the min and max lnLs may be inducing underflow")
@@ -831,13 +826,13 @@ determine.log.shift <- function(chunk.lnls){
 	return(max(unlist(chunk.lnls)))
 }
 
-#'@export
+
 shift.chunk.lnls <- function(chunk.lnls,A,n.iter){
 	shift.chunk.lnls <- log(sum(exp(chunk.lnls-A))) + A - log(n.iter)
 	return(shift.chunk.lnls)
 }
 
-#'@export
+
 calc.lnl.x.MCMC <- function(cov.chunk,pp.par.cov,chunk.size){
 	lnl.x.mcmc <- lapply(pp.par.cov,
 						function(x){
@@ -846,7 +841,7 @@ calc.lnl.x.MCMC <- function(cov.chunk,pp.par.cov,chunk.size){
 	return(unlist(lnl.x.mcmc))
 }
 
-#'@export
+
 x.validation <- function(test.pct,n.reps,K,freqs,geoDist,coords,prefix,n.iter){
 	x.val <- lapply(1:n.reps,
 					function(i){
@@ -863,7 +858,7 @@ x.validation <- function(test.pct,n.reps,K,freqs,geoDist,coords,prefix,n.iter){
 	return(x.val)
 }
 
-#'@export
+
 x.validation.rep <- function(rep.no,test.pct,K,freqs,geoDist,coords,prefix,n.iter,make.figs=FALSE,save.files=FALSE){
 	freqs <- drop.invars(freqs)
 	train.loci <- sample(1:ncol(freqs),ncol(freqs)*(1-test.pct))
@@ -910,12 +905,11 @@ x.validation.rep <- function(rep.no,test.pct,K,freqs,geoDist,coords,prefix,n.ite
 	return(test.lnl)
 }
 
-#'@export
+
 fit.to.test <- function(test.data,conStruct.results){
 	freq.data <- process.freq.data(test.data)
 	pp.par.cov <- post.process.par.cov(conStruct.results,
-										samples = (1 + conStruct.results$posterior$n.iter/2):
-															conStruct.results$posterior$n.iter)
+										samples = 1:conStruct.results$posterior$n.iter)
 	test.lnl <- lapply(pp.par.cov,
 						function(x){
 							log.likelihood(freq.data$obsCov,x$inv,x$log.det,freq.data$n.loci)
