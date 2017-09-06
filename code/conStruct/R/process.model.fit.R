b8f73c24ea97422f34ca317b04707601806004f8
Gideon Bradburd <gbradburd@ucdavis.edu>
2017-09-05 20:10:40 -0400

    documented pkg

--- code/conStruct/R/process.model.fit.R
+++ code/conStruct/R/process.model.fit.R
@@ -1,15 +1,15 @@
 get.conStruct.results <- function(data.block,model.fit,n.chains){
-	conStruct.results <- setNames(
-						lapply(1:n.chains,
-							function(i){
-								get.conStruct.chain.results(data.block,model.fit,i)
-							}),
-					  paste0("chain_",1:n.chains))
+	conStruct.results <- stats::setNames(
+							lapply(1:n.chains,
+								function(i){
+									get.conStruct.chain.results(data.block,model.fit,i)
+								}),
+						  paste0("chain_",1:n.chains))
 	return(conStruct.results)
 }
 
 get.MAP.iter <- function(model.fit,chain.no){
-	lpd <- get_logposterior(model.fit)
+	lpd <- rstan::get_logposterior(model.fit)
 	MAP.iter <- lapply(lpd,which.max)[[chain.no]]
 	return(MAP.iter)
 }
@@ -19,9 +19,9 @@ get.admix.props <- function(model.fit,chain.no,N,n.clusters){
 	admix.props <- array(1,dim=c(model.fit@sim$n_save[chain.no],N,n.clusters))
 	if(any(grepl("w",model.fit@model_pars))){
 		for(k in 1:n.clusters){
-			admix.props[,,k] <- extract(model.fit,
-										pars=unlist(lapply(1:N,function(j){sprintf("w[%s,%s]",j,k)})),
-										permuted=FALSE,inc_warmup=TRUE)[,chain.no,]
+			admix.props[,,k] <- rstan::extract(model.fit,
+											pars=unlist(lapply(1:N,function(j){sprintf("w[%s,%s]",j,k)})),
+											permuted=FALSE,inc_warmup=TRUE)[,chain.no,]
 		}
 	}
 	return(admix.props)
@@ -32,19 +32,19 @@ get.par.cov <- function(model.fit,chain.no,N){
 	for(i in 1:N){
 		for(j in 1:N){
 			my.par <- sprintf("parCov[%s,%s]",i,j)
-			par.cov[,i,j] <- extract(model.fit,pars=my.par,inc_warmup=TRUE,permuted=FALSE)[,chain.no,]
+			par.cov[,i,j] <- rstan::extract(model.fit,pars=my.par,inc_warmup=TRUE,permuted=FALSE)[,chain.no,]
 		}
 	}
 	return(par.cov)
 }
 
 get.nuggets <- function(model.fit,chain.no,N){
-	nuggets <- extract(model.fit,pars="nugget",inc_warmup=TRUE,permuted=FALSE)[,chain.no,]
+	nuggets <- rstan::extract(model.fit,pars="nugget",inc_warmup=TRUE,permuted=FALSE)[,chain.no,]
 	return(nuggets)
 }
 
 get.gamma <- function(model.fit,chain.no){
-	gamma <- extract(model.fit,pars="gamma",inc_warmup=TRUE,permuted=FALSE)[,chain.no,]
+	gamma <- rstan::extract(model.fit,pars="gamma",inc_warmup=TRUE,permuted=FALSE)[,chain.no,]
 	return(gamma)
 }
 
@@ -59,21 +59,21 @@ get.alpha.params <- function(model.fit,chain.no,cluster,n.clusters){
 	alpha.pars <- model.fit@model_pars[grepl("alpha",model.fit@model_pars)]
 	if(length(alpha.pars) !=0 ){
 		if(n.clusters > 1){
-			alpha.params <- setNames(
-								lapply(1:length(alpha.pars),
-										function(i){
-											extract(model.fit,
-													pars=paste0(alpha.pars[i],"[",cluster,"]"),
-													inc_warmup=TRUE,permuted=FALSE)[,chain.no,]
-										}),alpha.pars)
+			alpha.params <- stats::setNames(
+									lapply(1:length(alpha.pars),
+											function(i){
+												rstan::extract(model.fit,
+														pars=paste0(alpha.pars[i],"[",cluster,"]"),
+														inc_warmup=TRUE,permuted=FALSE)[,chain.no,]
+											}),alpha.pars)
 		} else {
-			alpha.params <- setNames(
-								lapply(1:length(alpha.pars),
-										function(i){
-											extract(model.fit,
-													pars=alpha.pars[i],
-													inc_warmup=TRUE,permuted=FALSE)[,chain.no,]
-										}),alpha.pars)		
+			alpha.params <- stats::setNames(
+									lapply(1:length(alpha.pars),
+											function(i){
+												rstan::extract(model.fit,
+														pars=alpha.pars[i],
+														inc_warmup=TRUE,permuted=FALSE)[,chain.no,]
+											}),alpha.pars)		
 		}
 	} else {
 		alpha.params <- get.null.alpha.params(model.fit@sim$n_save[chain.no])
@@ -81,21 +81,21 @@ get.alpha.params <- function(model.fit,chain.no,cluster,n.clusters){
 	return(alpha.params)
 }
 
-get.null.mu <- function(n.iter){
-	mu <- rep(0,n.iter)
-	return(mu)
+get.null.phi <- function(n.iter){
+	phi <- rep(0,n.iter)
+	return(phi)
 }
 
-get.cluster.mu <- function(model.fit,chain.no,cluster){
-	has.mu <- any(grepl("mu",model.fit@model_pars))
-	if(has.mu){
-		mu <- extract(model.fit,
-						pars=paste0("mu","[",cluster,"]"),
+get.cluster.phi <- function(model.fit,chain.no,cluster){
+	has.phi <- any(grepl("phi",model.fit@model_pars))
+	if(has.phi){
+		phi <- rstan::extract(model.fit,
+						pars=paste0("phi","[",cluster,"]"),
 						inc_warmup=TRUE,permuted=FALSE)[,chain.no,]
 	} else {
-		mu <- get.null.mu(model.fit@sim$n_save[chain.no])
+		phi <- get.null.phi(model.fit@sim$n_save[chain.no])
 	}
-	return(mu)
+	return(phi)
 }
 
 get.cov.function <- function(data.block){
@@ -116,12 +116,12 @@ get.cov.function <- function(data.block){
 			cov.func <- function(cluster.params,data.block){
 				return(cluster.params$alpha0 *  
 						exp(-(cluster.params$alphaD*data.block$geoDist)^cluster.params$alpha2) + 
-							cluster.params$mu)
+							cluster.params$phi)
 			}
 		}
 		if(!data.block$spatial){
 			cov.func <- function(cluster.params,data.block){
-				return(matrix(cluster.params$mu,nrow=data.block$N,ncol=data.block$N))
+				return(matrix(cluster.params$phi,nrow=data.block$N,ncol=data.block$N))
 			}
 		}
 	}
@@ -142,19 +142,19 @@ get.cluster.cov <- function(cluster.params,data.block,n.iter){
 get.cluster.params <- function(model.fit,data.block,chain.no,cluster,n.clusters,n.iter){
 	cluster.params <- list()
 	cluster.params <- get.alpha.params(model.fit,chain.no,cluster,n.clusters)
-	cluster.params[["mu"]] <- get.cluster.mu(model.fit,chain.no,cluster)
+	cluster.params[["phi"]] <- get.cluster.phi(model.fit,chain.no,cluster)
 	cluster.cov <- get.cluster.cov(cluster.params,data.block,n.iter)
 	cluster.params <- c(cluster.params,list("cluster.cov"=cluster.cov))
 	return(cluster.params)
 }
 
 get.cluster.params.list <- function(model.fit,data.block,chain.no,n.iter){
-	cluster.params <- setNames(
-						lapply(1:data.block$K,
-									function(i){
-										get.cluster.params(model.fit,data.block,chain.no,i,data.block$K,n.iter)
-									}),
-						paste("Cluster",1:data.block$K,sep="_"))
+	cluster.params <- stats::setNames(
+								lapply(1:data.block$K,
+											function(i){
+												get.cluster.params(model.fit,data.block,chain.no,i,data.block$K,n.iter)
+											}),
+								paste("Cluster",1:data.block$K,sep="_"))
 	cluster.params <- make.cluster.params.S3(cluster.params)
 	return(cluster.params)
 }
@@ -166,7 +166,7 @@ make.cluster.params.S3 <- function(cluster.params){
 }
 
 print.cluster.params <- function(cluster.params){
-	print(str(cluster.params,max.level=1))
+	print(utils::str(cluster.params,max.level=1))
 }
 
 index.MAP <- function(param,MAP.iter){
@@ -213,13 +213,13 @@ make.conStruct.results.S3 <- function(conStruct.results){
 }
 
 print.conStruct.results <- function(conStruct.results){
-	print(str(conStruct.results,max.level=1))
+	print(utils::str(conStruct.results,max.level=1))
 }
 
 get.conStruct.chain.results <- function(data.block,model.fit,chain.no){
 	n.iter <- get.n.iter(model.fit,chain.no)
 	posterior <- list("n.iter" = model.fit@sim$n_save[chain.no],
-					  "lpd" = get_logposterior(model.fit)[[chain.no]],
+					  "lpd" = rstan::get_logposterior(model.fit)[[chain.no]],
 					  "nuggets" = get.nuggets(model.fit,chain.no,data.block$N),
 					  "par.cov" = get.par.cov(model.fit,chain.no,data.block$N),
 					  "gamma" = get.gamma(model.fit,chain.no),
@@ -227,6 +227,8 @@ get.conStruct.chain.results <- function(data.block,model.fit,chain.no){
 					  "admix.proportions" = get.admix.props(model.fit,chain.no,data.block$N,data.block$K))
 	MAP.iter <- get.MAP.iter(model.fit,chain.no)
 	MAP <- lapply(posterior,function(X){index.MAP(X,MAP.iter)})
+	names(MAP)[[1]]  <- "index.iter"
+	MAP[["index.iter"]] <- MAP.iter
 	conStruct.results <- list("posterior" = posterior,"MAP" = MAP)
 	conStruct.results <- make.conStruct.results.S3(conStruct.results)
 	return(conStruct.results)
