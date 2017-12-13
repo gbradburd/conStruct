a5ceafeed4c2da4beea0c67fcf7b7e7ea40f4767
Gideon Bradburd <gbradburd@ucdavis.edu>
2017-12-13 16:20:57 -0500

    more fastStr runs

--- sims/structure/datasets/simK2/exe.fastStr.R
+++ sims/structure/datasets/simK2/exe.fastStr.R
@@ -2,18 +2,18 @@ setwd("/Applications/STRUCTURE/fastStructure")
 
 K <- 2
 infile <- "/Users/bradburd/Dropbox/conStruct/sims/structure/datasets/simK2/simK2"
-outfile <- paste0("/Users/bradburd/Dropbox/conStruct/sims/structure/datasets/simK2/simK2_est",K)
+outfile <- "/Users/bradburd/Dropbox/conStruct/sims/structure/datasets/simK2/simK2_estK"
 call <- paste0("python structure.py -K ",K," --input=",infile," --output=",outfile," --prior=logistic --format=str")
 system(call)
 
 K <- 3
 infile <- "/Users/bradburd/Dropbox/conStruct/sims/structure/datasets/simK2/simK2"
-outfile <- paste0("/Users/bradburd/Dropbox/conStruct/sims/structure/datasets/simK2/simK2_estK",K)
+outfile <- "/Users/bradburd/Dropbox/conStruct/sims/structure/datasets/simK2/simK2_estK"
 call <- paste0("python structure.py -K ",K," --input=",infile," --output=",outfile," --prior=logistic --format=str")
 system(call)
 
 K <- 4
 infile <- "/Users/bradburd/Dropbox/conStruct/sims/structure/datasets/simK2/simK2"
-outfile <- paste0("/Users/bradburd/Dropbox/conStruct/sims/structure/datasets/simK2/simK2_estK",K)
+outfile <- "/Users/bradburd/Dropbox/conStruct/sims/structure/datasets/simK2/simK2_estK"
 call <- paste0("python structure.py -K ",K," --input=",infile," --output=",outfile," --prior=logistic --format=str")
 system(call)
--- sims/structure/datasets/simK3/exe.fastStr.R
+++ sims/structure/datasets/simK3/exe.fastStr.R
@@ -2,18 +2,18 @@ setwd("/Applications/STRUCTURE/fastStructure")
 
 K <- 2
 infile <- "/Users/bradburd/Dropbox/conStruct/sims/structure/datasets/simK3/simK3"
-outfile <- paste0("/Users/bradburd/Dropbox/conStruct/sims/structure/datasets/simK3/simK3_estK",K)
+outfile <- "/Users/bradburd/Dropbox/conStruct/sims/structure/datasets/simK3/simK3_estK"
 call <- paste0("python structure.py -K ",K," --input=",infile," --output=",outfile," --prior=logistic --format=str")
 system(call)
 
 K <- 3
 infile <- "/Users/bradburd/Dropbox/conStruct/sims/structure/datasets/simK3/simK3"
-outfile <- paste0("/Users/bradburd/Dropbox/conStruct/sims/structure/datasets/simK3/simK3_estK",K)
+outfile <- "/Users/bradburd/Dropbox/conStruct/sims/structure/datasets/simK3/simK3_estK"
 call <- paste0("python structure.py -K ",K," --input=",infile," --output=",outfile," --prior=logistic --format=str")
 system(call)
 
 K <- 4
 infile <- "/Users/bradburd/Dropbox/conStruct/sims/structure/datasets/simK3/simK3"
-outfile <- paste0("/Users/bradburd/Dropbox/conStruct/sims/structure/datasets/simK3/simK3_estK",K)
+outfile <- "/Users/bradburd/Dropbox/conStruct/sims/structure/datasets/simK3/simK3_estK"
 call <- paste0("python structure.py -K ",K," --input=",infile," --output=",outfile," --prior=logistic --format=str")
 system(call)
