ec1bdc286583ecaee10fd88ca32887e4f6d5c3d2
Gideon Bradburd <bradburd@user-25c879.user.msu.edu>
2018-07-02 13:28:24 -0400

    moved bear conStruct xvalidation runs

---
+++ data/bears/conStruct/xvalidation/exe.xval.R
@@ -0,0 +1,30 @@
+library(conStruct)
+library(doParallel)
+
+x.validation <- function(test.pct,n.reps,K,freqs,geoDist,coords,prefix,n.iter){
+	x.val <- mclapply(1:n.reps,
+					function(i){
+						x.validation.rep(rep.no = i,
+										 test.pct,
+										 K,
+										 freqs,
+										 geoDist,
+										 coords,
+										 prefix,
+										 n.iter)
+					},mc.cores=6,mc.allow.recursive=FALSE,mc.preschedule=FALSE)
+	names(x.val) <- paste0("rep_",1:n.reps)
+	return(x.val)
+}
+
+
+load("../../data/bear.dataset.Robj")
+
+x.validation(test.pct = 0.10,
+			 n.reps = 10,
+			 K = 1:7,
+			 freqs = bear.dataset$sample.freqs,
+			 geoDist = fields::rdist.earth(bear.dataset$sample.coords),
+			 coords = bear.dataset$sample.coords,
+			 prefix = "bear_",
+			 n.iter = 5e3)
