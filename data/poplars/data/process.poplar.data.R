1706d2758d53d6e0178bc5c143c6d53d33d7ee30
Gideon Bradburd <bradburd@user-25c879.user.msu.edu>
2018-07-02 12:56:54 -0400

    moved poplar data files

--- data/poplars/data/process.poplar.data.R
+++ data/poplars/data/process.poplar.data.R
@@ -38,8 +38,9 @@ drainage.sample.sizes <- matrix(NA,nrow=length(drainages),ncol=ncol(poplar.indiv
 drainage.long <- numeric(length(drainages))
 drainage.lat <- numeric(length(drainages))
 drainage.sp <- numeric(length(drainages))
+poplar.sample.sizes <- numeric(length(drainages))
 
-# function for getting number of samples genotyped 
+# function for getting mean number of samples genotyped 
 #	in each drainage at each locus
 get.pop.sample.sizes <- function(ind.freqs,in.pop){
 	pop.sample.sizes <- rep(length(in.pop),ncol(ind.freqs)) - 
@@ -63,6 +64,7 @@ for(i in 1:length(drainages)){
 	}	else {
 		drainage.sp[i] <- "mixed"
 	}
+	poplar.sample.sizes[i] <- length(in.pop)
 }
 
 drainage.coords <- cbind(drainage.long,drainage.lat)
@@ -81,3 +83,97 @@ poplar.data <- list("freqs" = drainage.freqs,
 					"sp.ID" = drainage.sp)
 
 save(poplar.data,file="poplar.data.Robj")
+save(poplar.sample.sizes,file="poplar.sample.sizes.Robj")
+
+################################
+# make STRUCTURE and ADMIXTURE input data files
+################################
+
+################################
+#	structure
+################################
+make.ind.genos <- function(x,N){
+		geno <- numeric(N)
+	if(is.na(x)){
+		geno <- rep(-9,N)
+	} else {
+		geno[sample(1:N,x)] <- 1	
+	}
+	return(geno)
+}
+
+split.pop.into.inds <- function(pop.counts,pop.nChromo){
+	ind.geno <- apply(pop.counts,2,function(x){make.ind.genos(x,pop.nChromo)})
+	return(ind.geno)
+}
+
+make.structure.data.file <- function(counts,sample.sizes){
+	geno.matrix <- matrix(NA,sum(sample.sizes),ncol(freqs))
+	index <- 0
+	for(n in 1:nrow(freqs)){
+		geno.matrix[index+c(1:sample.sizes[n]),] <- split.pop.into.inds(counts[n,,drop=FALSE],sample.sizes[n])
+		index <- index + sample.sizes[n]
+	}
+	PopData <- unlist(lapply(1:length(sample.sizes),
+					function(n){
+						lapply(1:sample.sizes[n],
+							function(i){
+								sprintf("ind_%s.%s",n,i)
+							})
+					}))
+	LocData <- unlist(lapply(1:length(sample.sizes),function(n){rep(n,sample.sizes[n])}))
+	return(cbind(PopData,LocData,geno.matrix))
+}
+
+freqs <- poplar.data$freqs
+sample.sizes <- poplar.sample.sizes*2
+counts <- freqs*matrix(sample.sizes,nrow(freqs),ncol(freqs))
+str.data <- make.structure.data.file(counts,sample.sizes)
+write.table(str.data,file="poplars.str",quote=FALSE,row.names=FALSE,col.names=FALSE)
+
+
+################################
+#	admixture
+################################
+convert.str.to.ped <- function(str.file){
+	str <- read.table(str.file,header=FALSE,stringsAsFactors=FALSE,row.names=1)
+	n.loci <- ncol(str)-1
+	n.haps <- nrow(str)
+	hap.IDs <- row.names(str)
+	pop.IDs <- as.numeric(unlist(lapply(lapply(strsplit(hap.IDs,"_"),"[[",2),function(x){strsplit(x,"\\.")[[1]][[1]]}))[seq(1,n.haps,by=2)])
+	mand.cols <- matrix(0,nrow=n.haps/2,ncol=6)
+	#family
+	mand.cols[,1] <- pop.IDs
+	#ind ID
+	mand.cols[,2] <- unlist(lapply(unique(pop.IDs),function(x){1:length(which(pop.IDs==x))}))
+	genos <- matrix(NA,nrow=n.haps/2,ncol=n.loci*2)
+	genos[,seq(1,n.loci*2,by=2)] <- as.matrix(str[seq(1,n.haps,by=2),2:ncol(str)])
+	genos[,seq(2,n.loci*2,by=2)] <- as.matrix(str[seq(2,n.haps,by=2),2:ncol(str)])
+	genos <- genos + 1
+	ped <- cbind(mand.cols, genos)
+	return(ped)
+}
+
+make.map.file <- function(ped){
+	#recover()
+	n.snps <- (ncol(ped)-6)/2
+	map <- matrix(NA,nrow=n.snps,ncol=3)
+	#CHR
+	map[,1] <- rep(1,n.snps)
+	#RS
+	map[,2] <- paste0("rs",1:n.snps)
+	#BP
+	map[,3] <- seq(1,n.snps*1e3,length.out=n.snps)
+	return(map)
+}
+
+ped_poplar <- convert.str.to.ped(str.file="poplars.str")
+map_poplar <- make.map.file(ped_poplar)
+	write.table(ped_poplar,file="../admixture/poplar.ped",sep="\t",quote=FALSE,row.names=FALSE,col.names=FALSE)
+	write.table(map_poplar,file="../admixture/poplar.map",sep="\t",quote=FALSE,row.names=FALSE,col.names=FALSE)
+call <- "plink --noweb --file ../admixture/poplar --make-bed --out ../admixture/poplar --map3 --allow-no-sex"
+system(call)
+file.remove("../admixture/poplar.ped")
+file.remove("../admixture/poplar.map")
+file.remove("../admixture/poplar.log")
+file.remove("../admixture/poplar.nosex")
