0ac5202be739c4d8acb98a18f0e3dea4f28969dc
gbradburd <bradburd@umich.edu>
2026-08-09 13:44:44 -0400

    test sandbox

--- testing/xval/test.xval.R
+++ testing/xval/test.xval.R
@@ -1,19 +1,34 @@
 library(conStruct)
-load("sim.dataset.Robj")
+#load("sim.dataset.Robj")
+data(conStruct.data)
 
-options(error=recover)
+#options(error=recover)
 
 xvals <- x.validation(train.prop = 0.9,
-					  n.reps = 2,
-					  K = 1:2,
-					  freqs = sim.dataset$freqs,
-					  geoDist = fields::rdist(sim.dataset$coords),
-					  coords = sim.dataset$coords,
+					  n.reps = 4,
+					  K = 1:3,
+					  freqs = conStruct.data$allele.frequencies,
+					  geoDist = conStruct.data$geoDist,
+					  coords = conStruct.data$coords,
 					  prefix = "xval_test1",
 					  n.iter = 1e3,
 					  make.figs = FALSE,
 					  save.files = FALSE,
-					  parallel = FALSE,
-					  n.nodes = NULL)
+					  parallel = TRUE,
+					  n.nodes = 6)
 
 save(xvals,file="test1.xvals.Robj")
+
+
+# xvals <- x.validation(train.prop = 0.9,
+					  # n.reps = 4,
+					  # K = 1:3,
+					  # freqs = sim.dataset$freqs,
+					  # geoDist = fields::rdist(sim.dataset$coords),
+					  # coords = sim.dataset$coords,
+					  # prefix = "xval_test1",
+					  # n.iter = 1e3,
+					  # make.figs = FALSE,
+					  # save.files = FALSE,
+					  # parallel = FALSE,
+					  # n.nodes = NULL)
